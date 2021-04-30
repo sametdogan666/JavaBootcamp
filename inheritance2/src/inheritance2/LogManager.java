ef27945030e62d35905b83b9d1147f7d0b015d05
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-04-30 20:44:29 +0300

    30 Nisan Commit 4

---
+++ inheritance2/src/inheritance2/LogManager.java
@@ -0,0 +1,19 @@
+package inheritance2;
+
+public class LogManager {
+
+	public void log(int logType) {
+		if (logType == 1) {
+			System.out.println("Veritabanına loglandı");
+		} else if (logType == 2) {
+			System.out.println("Dosyaya loglandı");
+		} else {
+			System.out.println("Email gönderildi");
+		}
+	}
+
+}
+
+//1 - Database
+//2 - File
+//3 - Email
